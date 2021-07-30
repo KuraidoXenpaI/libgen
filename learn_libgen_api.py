2bae4fc094a697824ae11f806d81cc43b89a9c5e
KuraidoXenpaI <asdwera996@gmail.com>
2021-07-31 00:46:55 +0800

    Update learn_libgen_api.py

--- learn_libgen_api.py
+++ learn_libgen_api.py
@@ -3,25 +3,31 @@ import pprint
 import requests
 import os
 
-os.chdir('D:\Cs\Python\libgen_api')
+# os.chdir('D:\Cs\Python\libgen_api')
+os.chdir('Download')
 
 printer = pprint.PrettyPrinter()
 s = LibgenSearch()
 
 def get_book():
+    # Prompts User to searcha book titl
     book_title = input('Book Title: ')
 
+    # prints the results
     results = s.search_title(book_title)
     for result in results:
         printer.pprint(result)
         print()
 
+    #  prompts the user to select a book from the selection
     select = input('Select a book from the results: ')
     os.system('cls')
 
-    item_to_dowload = results[int(select)]
+    # fetches download links from the request
+    item_to_dowload = results[int(select) - 1]
     download_links = s.resolve_download_links(item_to_dowload)
 
+    # prompts user to select download methods
     print('\nDownload Methods: ')
     for key, value in download_links.items():
         print(key)
@@ -29,12 +35,13 @@ def get_book():
     select = input("\nSelect Download Method: ")
     os.system('cls')
 
+    # send an api request for downloading the book
     url = download_links[select]
     request = requests.get(url)
 
-    filename = input('Save as: ')
-    filename += '.' + item_to_dowload['Extension']
+    filename = f'{item_to_dowload["Title"]}.{item_to_dowload["Extension"]}'
 
+    # saves the contents into file
     file = open(filename, 'wb')
     file.write(request.content)
     file.close()
